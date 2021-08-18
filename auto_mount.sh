0d9aa432d54892029cc11c104888a9683aa95cbe
Mohammed Julfikar Ali Mahbub <39207213+o-julfikar@users.noreply.github.com>
2021-08-19 03:23:10 +0600

    Create auto_mount.sh

---
+++ auto_mount.sh
@@ -0,0 +1,20 @@
+#!/binbash
+
+sudo mkfs.xfs -f -L d1 /dev/sda
+sudo mkfs.xfs -f -L d2 /dev/sdb
+sudo mkfs.xfs -f -L d3 /dev/sdc
+
+
+sudo mkdir -p /srv/node/d1
+sudo mkdir -p /srv/node/d2
+sudo mkdir -p /srv/node/d3
+
+
+sudo useradd swift
+sudo chown -R swift:swift /srv/node
+
+sudo cp /opt/cloud/mount_devices.sh /opt/swift/bin/mount_devices.sh
+
+sudo cp /opt/cloud/start_swift.service /etc/systemd/system/start_swift.service
+
+echo Thank you! Reboot now.
