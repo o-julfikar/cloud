c39d19f13f27d094218118fec14d6dfedb3007cf
Mohammed Julfikar Ali Mahbub <39207213+o-julfikar@users.noreply.github.com>
2021-08-19 06:02:22 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo Installing dependencies...
+sudo apt update
+sudo apt install curl gcc memcached rsync sqlite3 xfsprogs \
+                     git-core libffi-dev python-setuptools \
+                     liberasurecode-dev libssl-dev
+sudo apt install python-coverage python-dev python-nose \
+                     python-xattr python-eventlet \
+                     python-greenlet python-pastedeploy \
+                     python-netifaces python-pip python-dnspython \
+                     python-mock
+
 cd /opt 
 sudo git clone https://github.com/openstack/python-swiftclient.git
 cd /opt/python-swiftclient
