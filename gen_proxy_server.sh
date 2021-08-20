2ed0329a51d84c649dc9023a5b6a17d99b735d22
Mohammed Julfikar Ali Mahbub <39207213+o-julfikar@users.noreply.github.com>
2021-08-20 10:07:54 +0600

    Update gen_proxy_server.sh

--- gen_proxy_server.sh
+++ gen_proxy_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 usercred="user_${USER}_me = secretpassword .admin .reseller_admin"
 sudo cp back-up-proxy-server.conf proxy-server.conf
-sudo sed -i -e 's/usercred/$usercred/g' proxy-server.conf
+sudo sed -i -e "s/usercred/$usercred/g" proxy-server.conf
 
