d46dad4d436a9bbea1036f2029c9d5b8fc616195
Mohammed Julfikar Ali Mahbub <39207213+o-julfikar@users.noreply.github.com>
2021-08-20 09:54:07 +0600

    Update gen_proxy_server.sh

--- gen_proxy_server.sh
+++ gen_proxy_server.sh
@@ -3,12 +3,12 @@ sudo touch proxy-server.conf
 sudo chmod 777 proxy-server.conf
 sudo bash -c "echo '' > proxy-server.conf"
 i=0
-input="/opt/cloud/back-up-proxy-server.conf"
+file="/opt/cloud/back-up-proxy-server.conf"
 while IFS= read -r line
 do
   "$line" >> proxy-server.conf
-  if [[ $i -eq 406 ]]; then;
+  if [[ $i -eq 406 ]]; then
     echo "user_${USER}_me = secretpassword .admin .reseller_admin"
   fi
   i=$(( $i + 1 ))
-done
+done < "$file"
