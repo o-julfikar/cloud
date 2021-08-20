a3f74954e6b01e03c26fb59395f408babdba3104
Mohammed Julfikar Ali Mahbub <39207213+o-julfikar@users.noreply.github.com>
2021-08-20 09:47:47 +0600

    Update gen_proxy_server.sh

--- gen_proxy_server.sh
+++ gen_proxy_server.sh
@@ -7,8 +7,8 @@ input="/opt/cloud/back-up-proxy-server.conf"
 while IFS= read -r line
 do
   "$line" >> proxy-server.conf
-  if [ $i -eq 406 ]; then;
+  if [[ $i -eq 406 ]]; then;
     echo "user_${USER}_me = secretpassword .admin .reseller_admin"
   fi
-  i=$(( $i + 1))
+  i=$(( $i + 1 ))
 done
